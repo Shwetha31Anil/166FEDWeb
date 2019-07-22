5333493c2328764f48215b662e8ad6c1f6889d1e
i.kumar <i.kumar@WKSBAN24TR17010.corp.mphasis.com>
2019-07-22 12:21:37 +0530

    Mohit1 javva beans

--- FirstWebApplication/src/com/mphasis/cart/beans/Mohit1.java
+++ FirstWebApplication/src/com/mphasis/cart/beans/Mohit1.java
@@ -1,5 +1,13 @@
 package com.mphasis.cart.beans;
 
 public class Mohit1 {
+private String name;
 
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
 }
